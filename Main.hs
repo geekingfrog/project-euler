d56fb612d87384e1e28e2d5b8b4b9fa91d97756e
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2015-08-25 05:44:58 +0100

    Problem 28

--- Main.hs
+++ Main.hs
@@ -27,6 +27,7 @@ import Problem024 as P24
 import Problem025 as P25
 import Problem026 as P26
 import Problem027 as P27
+import Problem028 as P28
 import Problem067 as P67
 
 main :: IO ()
@@ -67,5 +68,6 @@ runSolution 24 = return $ show P24.answer
 runSolution 25 = return $ show P25.answer
 runSolution 26 = return $ show P26.answer
 runSolution 27 = return $ show P27.answer
+runSolution 28 = return $ show P28.answer
 runSolution 67 = P67.answer >>= (return . show)
 runSolution _ = undefined
---
+++ Problem028.hs
@@ -0,0 +1,16 @@
+module Problem028 (answer) where
+
+answer :: Int
+answer = 1 + (sum $ take ((s-1)*2) $ pick [2..])
+  where s = 1001
+
+-- pick number on diagonals means taking 1 number every 2n
+-- n ranging from 0 to (s-1)/2 with s the side of the square
+pick :: [Int] -> [Int]
+pick xs = go 1 1 4 xs
+  where
+    go _ _ _ [] = undefined
+    go n nRemain totalRemain (x:xs)
+      | totalRemain == 0 = go (n+1) (2*n) 4 xs
+      | nRemain == 0 = x : go n (2*n-1) (totalRemain-1) xs
+      | otherwise = go n (nRemain-1) totalRemain xs
