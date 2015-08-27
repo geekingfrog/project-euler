cd58e0d2353dc00fcb567add163e40c5516623a7
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2015-08-27 19:43:36 +0100

    Improved version

--- Problem036.hs
+++ Problem036.hs
@@ -1,9 +1,24 @@
 module Problem036 (answer) where
 
-import NumUtil (decompose, toBase)
+import Data.List (nub)
+import NumUtil (decompose, recompose, toBase)
 
 answer :: Int
-answer = sum [i | i <- [1..1000000], isPalindrome $ decompose i, isPalindrome $ (toBase 2 i)]
+answer = sum $ map recompose $ filter (isPalindrome . (toBase 2) . recompose) palindroms
+  where
+    rawpalindroms = concat [[makePalindrome i False, makePalindrome i True] | i <- [1..999]]
+    palindroms = nub $ filter ((>0) . length) $ map filterZeros rawpalindroms
+    filterZeros [] = []
+    filterZeros (0:xs) = init xs
+    filterZeros xs = xs
 
 isPalindrome :: (Eq a) => [a] -> Bool
 isPalindrome x = (reverse x) == x
+
+makePalindrome :: Int -> Bool -> [Int]
+makePalindrome x shifted = go shifted decomposed decomposed
+  where
+    decomposed = reverse $ decompose x
+    go _ [] acc = acc
+    go False (x:xs) acc = go False xs (x:acc)
+    go True  (_:xs) acc = go False xs acc
