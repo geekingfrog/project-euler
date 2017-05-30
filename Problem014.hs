8795ff58c61ac63cac95403ccc6f05e82080a355
Grégoire Charvet 黑瓜 <greg+git@geekingfrog.com>
2017-05-30 13:42:58 +0100

    Arrows !!!!

--- Problem014.hs
+++ Problem014.hs
@@ -1,12 +1,13 @@
 module Problem014 (answer) where
 
+import Control.Arrow ((&&&))
 import Data.List
 import Data.Function (on)
 
 answer :: Int
 answer = fst $ maximumBy (compare `on` snd) chains
   where
-    chains = map (\x -> (x, 1 + length (collatz x))) [1..1000000]
+    chains = map (id &&& (length . collatz)) [1..1000000]
 
 collatz :: Int -> [Int]
 collatz seed = takeWhile (/= 1) $ iterate go seed
